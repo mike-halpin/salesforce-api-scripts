df8c4ce3b9f0f6c8b935932ba33a00abb5591a90
Michael Halpin <g+ithub@halpin.io>
2023-07-20 08:35:20 -0400

    Five passing tests for soql_statement

--- soql/soql_statement.py
+++ soql/soql_statement.py
@@ -1,7 +1,12 @@
-class SOQLStatement:
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from abc import ABC, abstractmethod
+
+class SOQLStatement(ABC):
     def __init__(self, object_name, fields=[]):
         self.object_name = object_name
-        self.fields = field
+        self.fields = fields
         self.where_conditions = []
         self.order_by_fields = []
 
@@ -14,31 +19,56 @@ class SOQLStatement:
             self.fields.remove(field)
 
     def add_condition(self, field, operator, value):
-        condition = f"{field} {operator} {repr(value)}"
-        self.where_conditions.append(condition)
-
+        self.where_conditions.append((field, operator, value))
+    
+    @abstractmethod
+    def construct_query(self):
+        pass
+    
+    def __str__(self):
+        return self.construct_query()
 
 class SelectSOQLStatement(SOQLStatement):
     def __init__(self, object_name, fields):
-        super().__init__(object_name)
-
-    def add_condition(self, field, operator, value):
-        self.where_conditions.append((field, operator, value))
-
+        super().__init__(object_name, fields)
+        
     def add_order_by(self, field, direction='ASC'):
         self.order_by_fields.append((field, direction))
 
-    def construct_query(self):
-        fields_str = ', '.join(self.fields)
-        query = f"SELECT {fields_str} FROM {self.object_name}"
-
+    def echo_where_clause(self):
+        where_str = ''
         if self.where_conditions:
             where_str = ' AND '.join([f"{field} {operator} {value}" for field, operator, value in self.where_conditions])
-            query += f" WHERE {where_str}"
+            where_str = f"WHERE {where_str}"
+            print(f"{where_str}")
 
+        return where_str
+
+    def echo_select_clause(self):
+        fields_str = ''
+        if self.fields:
+            fields_str = ', '.join(self.fields)
+            fields_str = f"SELECT {fields_str}"
+            print(f"{fields_str}")
+
+        return fields_str
+
+    def echo_from_clause(self):
+        from_clause = f"FROM {self.object_name}"
+        print(f"{from_clause}")
+
+        return from_clause
+
+    def echo_order_by_clause(self):
+        order_by_str = ''
         if self.order_by_fields:
             order_by_str = ', '.join([f"{field} {direction}" for field, direction in self.order_by_fields])
-            query += f" ORDER BY {order_by_str}"
+            order_by_str = f"ORDER BY {order_by_str}"
+            print(f"{order_by_str}")
+        return order_by_str
+
+    def construct_query(self):
+        query = f"{self.echo_select_clause()} {self.echo_from_clause()} {self.echo_where_clause()} {self.echo_order_by_clause()}"
 
         return query
 
--- soql/tests/test_soql_statement.py
+++ soql/tests/test_soql_statement.py
@@ -1,33 +1,36 @@
 import unittest
-
+import re
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from ..soql_statement import SOQLStatement, SelectSOQLStatement
 
 
 class TestSOQLStatement(unittest.TestCase):
     def test_add_field(self):
-        statement = SOQLStatement('Object')
-        statement.add_field('Field1')
-        self.assertIn('Field1', statement.fields)
+        statement = SelectSOQLStatement('Object', ['Field1', 'Field2'])
+        statement.add_field('TEST')
+        self.assertIn('TEST', statement.fields)
 
     def test_remove_field(self):
-        statement = SOQLStatement('Object', ['Field1', 'Field2'])
+        statement = SelectSOQLStatement('Object', ['Field1', 'Field2'])
         statement.remove_field('Field1')
         self.assertNotIn('Field1', statement.fields)
 
     def test_add_condition(self):
-        statement = SOQLStatement('Object')
+        statement = SelectSOQLStatement('Object', ['Field1', 'Field2'])
         statement.add_condition('Field1', '=', 10)
-        self.assertEqual(statement.where_conditions, ["Field1 = 10"])
+        self.assertEqual(re.sub('%s', statement.__str__(), ''), re.sub('%s', "SELECT Field1, Field2 FROM Object WHERE Field1 = 10", ''))
 
     def test_construct_query_without_conditions_and_order_by(self):
-        statement = SOQLStatement('Object', ['Field1', 'Field2'])
+        statement = SelectSOQLStatement('Object', ['Field1', 'Field2'])
         query = statement.construct_query()
-        self.assertEqual(query, "SELECT Field1, Field2 FROM Object")
+        self.assertEqual(re.sub('%s', query, ''), re.sub('%s', "SELECT Field1, Field2 FROM Object", ''))
 
     def test_construct_query_with_conditions_and_order_by(self):
         statement = SelectSOQLStatement('Object', ['Field1', 'Field2'])
         statement.add_condition('Field1', '=', 10)
-        statement.add_order_by('Field2', 'DESC')
+        statement.add_order_by('Field2', direction='DESC')
         query = statement.construct_query()
         expected_query = "SELECT Field1, Field2 FROM Object WHERE Field1 = 10 ORDER BY Field2 DESC"
         self.assertEqual(query, expected_query)
@@ -35,4 +38,3 @@ class TestSOQLStatement(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
